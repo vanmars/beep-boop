e2181a27cfbfd467dd01c0e7b71b81d341ff070d
Vanessa Stewart <vstewart1213@gmail.com>
2020-08-21 09:15:53 -0700

    Create main beepBoop function, apply test edits, and pass TEST 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 // Business Logic
+function beepBoop (number) {
+  // alert("Please enter a number, and use digits rather than letters.");
+  return "Please enter a number, and use digits rather than letters.";
+};
 
 // Tests
   // Test 1 - Check for non-numbers
@@ -7,22 +11,22 @@
   console.log("Returned: " + (beepBoop("thirty")));
 
   // Test 2 - Return range of numbers
-  console.log("Test 1: " + (beepBoop(4) === "0, 1, 2, 3, 4"));
+  console.log("Test 2: " + (beepBoop(4) === "0, 1, 2, 3, 4"));
   console.log("Expected: 0, 1, 2, 3, 4.")
   console.log("Returned: " + (beepBoop(4)));
 
   // Test 3 - Return 'Beep'
-  console.log("Test 1: " + (beepBoop(123) === "Beep"));
+  console.log("Test 3: " + (beepBoop(123) === "Beep"));
   console.log("Expected: Beep")
   console.log("Returned: " + (beepBoop(123)));
 
   // Test 4 - Return 'Boop'
-  console.log("Test 1: " + (beepBoop(123) === "Boop"));
+  console.log("Test 4: " + (beepBoop(123) === "Boop"));
   console.log("Expected: Boop")
   console.log("Returned: " + (beepBoop(123)));
 
   // Test 5 - Return 'Won't You Be My Neighbor?'
-  console.log("Test 1: " + (beepBoop(123) === "Won't you be my neighbor?"));
+  console.log("Test 5: " + (beepBoop(123) === "Won't you be my neighbor?"));
   console.log("Expected: Won't you be my neighbor?")
   console.log("Returned: " + (beepBoop(123)));
 
