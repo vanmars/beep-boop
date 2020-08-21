13d61a7528ed1477c1ba8f9333957c81e7e9a99d
Vanessa Stewart <vstewart1213@gmail.com>
2020-08-21 09:51:28 -0700

    Pass TEST 3

--- js/scripts.js
+++ js/scripts.js
@@ -30,14 +30,13 @@ function containsOne (number) {
   return false;
 };
 
-
 function beepBoop (number) {
   let result = [];
   if (isNaN(number)){ 
   // alert("Please enter a number, and use digits rather than letters.");
   return "Please enter a number, and use digits rather than letters."
-  // } else if (containsThree(number)) {
-  //   return ""
+  } else if (containsOne(number)) {
+    return "Beep"
   } else {
     for (i=0; i<=number; i++){
       result.push(i);
