5a88f97dd790a37c8233fd7eb141af18562a977f
Vanessa Stewart <vstewart1213@gmail.com>
2020-08-21 09:03:02 -0700

    Add all tests

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,29 @@
-// SPECIFICATIONS
-// 1. Behavior: The program returns a range of numbers from 0 to the user's inputted number.
-// * Input: "thirty"
-// * Output: "Please enter a number, and use _digits_ rather than letters."
-
-// 2. Behavior: The program returns a range of numbers from 0 to the user's inputted number.
-// * Input: "4"
-// * Output: "0, 1, 2, 3, 4"
-
-// 3. Behavior: If the user's inputted number contains a 1, the program returns "Beep."
-// * Input: "123"
-// * Output: "Beep"
-
-// 4. Behavior: If the user's inputted number contains a 2, the program returns "Boop."
-// * Input: "123"
-// * Output: "Boop"
-
-// 5. Behavior: If the user's inputted number contains a 3, the program returns "Won't you be my neighbor?".
-// * Input: "123"
-// * Output: "Won't you be my neighbor?"
-
 // Business Logic
 
 // Tests
+  // Test 1 - Check for non-numbers
+  console.log("Test 1: " + (beepBoop("thirty") === "Please enter a number, and use digits rather than letters."));
+  console.log("Expected: Please enter a number, and use digits rather than letters.")
+  console.log("Returned: " + (beepBoop("thirty")));
+
+  // Test 2 - Return range of numbers
+  console.log("Test 1: " + (beepBoop(4) === "0, 1, 2, 3, 4"));
+  console.log("Expected: 0, 1, 2, 3, 4.")
+  console.log("Returned: " + (beepBoop(4)));
+
+  // Test 3 - Return 'Beep'
+  console.log("Test 1: " + (beepBoop(123) === "Beep"));
+  console.log("Expected: Beep")
+  console.log("Returned: " + (beepBoop(123)));
+
+  // Test 4 - Return 'Boop'
+  console.log("Test 1: " + (beepBoop(123) === "Boop"));
+  console.log("Expected: Boop")
+  console.log("Returned: " + (beepBoop(123)));
+
+  // Test 5 - Return 'Won't You Be My Neighbor?'
+  console.log("Test 1: " + (beepBoop(123) === "Won't you be my neighbor?"));
+  console.log("Expected: Won't you be my neighbor?")
+  console.log("Returned: " + (beepBoop(123)));
 
 // User Interface Logic
